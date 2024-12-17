e9bdfe7faf65aa3bf85615408524c26edcc9b7ad
Rishav Gayen <gayenrishav19@gmail.com>
2024-12-17 15:47:19 +0000

    Added Final Functionality

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ function reattachEventListeners() {
       } else {
         button.textContent = 'Read';
       }
+      storeElement();
     });
-    storeElement();
   });
 
   removeBookButtons.forEach(button => {
